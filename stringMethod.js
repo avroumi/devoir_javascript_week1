c5411c104d73e6c557c70bc48f458891fe3c7180
Avroumi <avroumi97@gmail.com>
2026-06-23 15:58:27 +0300

    5-10

--- stringMethod.js
+++ stringMethod.js
@@ -9,3 +9,14 @@ console.log(str1.toUpperCase())
 const str3 = "HELLO WORLD"
 console.log(str3.toLowerCase())
 
+const str4 = "Javascript"
+console.log(str4.slice(0, 4))
+
+console.log(str4.slice(4,))
+
+const str5 = "https://example.com"
+console.log(str5.startsWith("https"))
+
+console.log(str5.endsWith(".com"))
+
+console.log(str1.replace("hello", "hi"))
