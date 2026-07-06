d23cf57d7c1351c7c200886f8b64c22269cc7404
avroumi <avroumi97@gmail.com>
2026-07-06 17:50:58 +0300

    add data json and crud not finalyse just getall getbyid and post , end of the day

---
+++ shavouaThreeJS/superHeroProject/data/heroesData.js
@@ -0,0 +1,18 @@
+import fs from "fs/promises"
+
+const FILE_PATH = "./data/heroes.json"
+
+export const readHeroes = async () => {
+    const res = await fs.readFile(FILE_PATH,"utf-8")
+    const data = JSON.parse(res)
+    return  data
+}
+
+export const writeHeroes = async (data) => {
+    
+    const jsonData = JSON.stringify(data, null , 2)
+    await fs.writeFile(FILE_PATH, data, "utf-8")
+    return {message : "Write succesfully"}
+    
+}
+
