e4c4df4c396d643bb9f7e0c91a8819adff71931d
avroumi <avroumi97@gmail.com>
2026-07-07 11:43:09 +0300

    add routes crud with vanilla http

---
+++ shavouaThreeJS/superHeroProject/routes/heroes.routes.js
@@ -0,0 +1,51 @@
+import { getAllHeroes, getHeroesById, 
+    updateHero, deleteHero, createHero } from "../services/heroes.service.js";
+import { parseBody } from "../utils/parseBody.js";
+import { sendError,sendSuccess } from "../utils/response.js";
+
+export const handleHeroesRoutes = async (req, res, url) => {
+    const {method} = req
+    const pathname = url.pathname
+
+    try{
+        if (method === "GET" && pathname === "/heroes") {
+            const heroes = await getAllHeroes()
+            sendSuccess(res, 200, heroes)
+            return true
+        }
+        if (method === "POST" && pathname === "/heroes"){
+            const body = await parseBody(req)
+            const newHero = await createHero(body)
+            sendSuccess(res, 201, newHero)
+            return true 
+        }
+
+        const parts = pathname.split("/").filter(Boolean)
+
+        if (method === "GET" && parts[0] === "heroes" && parts[1] && parts.length === 2){
+            const hero = await getHeroesById(parts[1])
+            sendSuccess(res, 200, hero)
+            return true 
+        }
+
+        if (method === "PATCH" && parts[0] === "heroes" && parts[1] && parts.length === 2){
+            const body = await parseBody(req)
+            const updatedHero = await updateHero(parts[1], body)
+            sendSuccess(res , 200 , updatedHero)
+            return true
+        }
+        if (method === "DELETE" && parts[0] === "heroes" && parts[1] && parts.length === 2){
+            const deletedHero = await deleteHero(parts[1])
+            sendSuccess(res, 200, deletedHero)
+            return true
+        }
+        return false
+
+    }catch(error) {
+        const statusCode = error.statusCode || 500
+        const message = error.message || "Internal server error"
+
+        sendError(res, statusCode, message)
+        return true 
+    }
+}
