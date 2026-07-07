bdc33b86d16dd397017ea225034a7f11c2758eda
avroumi <avroumi97@gmail.com>
2026-07-07 12:05:16 +0300

    add crud in server.js

--- shavouaThreeJS/superHeroProject/server.js
+++ shavouaThreeJS/superHeroProject/server.js
@@ -2,15 +2,20 @@ import http from "http"
 import {URL} from "url"
 import { sendError,sendSuccess } from "./utils/response.js"
 import {notFound} from "./middlewares/notFound.js"
+import { handleHeroesRoutes } from "./routes/heroes.routes.js"
 
 
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const {method} = req
     const url = new URL(req.url, 'http://localhost')
 
     if (method === "GET" && url.pathname === "/health"){
         return sendSuccess(res, 200,"Hello to healthy server")
+    }
+    const handledHeroes = await handleHeroesRoutes(req, res, url)
+    if (handledHeroes){
+        return 
     }
     return notFound(res)
 
