292995d73b45d0152dc1aafba5b27f358f16b0d8
avroumi <avroumi97@gmail.com>
2026-07-13 21:22:22 +0300

    add products service with get all and get by id

---
+++ shavouaThreeJS/tamim-leolamim-store/src/services/products.service.js
@@ -0,0 +1,48 @@
+import { readProducts } from "../repositories/products.repository.js";
+import AppError from "../utils/AppError.js"
+
+export const getAllproducts = async (filters) => {
+    const {inStock, maxPrice,search} =filters
+
+    let products = await readProducts()
+
+    if (inStock !== undefined){
+       if  (inStock !== "true" && inStock !== "false"){
+    
+        throw new AppError("inStock must be true or false", 400 )
+       }
+    if (inStock === "true"){
+        products = products.filter(product=> product.stock > 0 )
+    }
+    
+    } if (maxPrice !== undefined){
+        const priceLimit = Number(maxPrice)
+
+    if (Number.isNaN(priceLimit) || priceLimit < 0){
+        throw new AppError("maxPrice must be a integrer anv positive number ", 400)
+    }
+    products = products.filter(product=> product.price <= priceLimit)
+    }
+    if (search !== undefined){
+        const searchText  = search.toLowercase()
+
+        products = products.filter(product => product.name.toLowercase().includes(searchText))
+    }
+
+    return products
+}
+
+
+
+
+
+export const getProductById = async (productId) => {
+    const products = await readProducts()
+
+    const product = products.find(product => product.id === productId)
+    if (!product){
+        throw new AppError("Product not found ", 404)
+    }
+    return product
+}
+
